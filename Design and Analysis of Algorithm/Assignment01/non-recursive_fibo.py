f9399800057cd9335a19bbc770e96c99e94a1716
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2023-10-13 15:11:49 +0530

    Update non-recursive_fibo.py

--- Design and Analysis of Algorithm/Assignment01/non-recursive_fibo.py
+++ Design and Analysis of Algorithm/Assignment01/non-recursive_fibo.py
@@ -6,16 +6,17 @@ def fibonacci(n):
     if n == 1:
         return 1
     else:
-        a,b = 0,1
-        for i in range(2,n+1):
-            a,b = b , a + b
-        return b
-    
+        a,b = 0,1               # Initialize the first two Fibonacci numbers
+        for i in range(2,n+1):  # Start the loop from the third Fibonacci number
+            a,b = b , a + b     # Update a and b to calculate the next Fibonacci number
+        return b                # Return nth fibonacci number
+
+# Input the value of n from the user   
 n = int(input("Enter value of n: "))
 if n<0:
     print("Enter non- negative number")
 else:
-    result = fibonacci(n)
+    result = fibonacci(n)       # Call the fibonacci function to calculate the Fibonacci number
     print("The Fibonacci number is ",result)
 
 
