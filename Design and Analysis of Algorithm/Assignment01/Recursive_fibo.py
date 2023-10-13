a402f585de0519c4903d0787f35d668a0e82594c
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2023-10-13 15:13:19 +0530

    Update Recursive_fibo.py

--- Design and Analysis of Algorithm/Assignment01/Recursive_fibo.py
+++ Design and Analysis of Algorithm/Assignment01/Recursive_fibo.py
@@ -1,19 +1,21 @@
 # RECURSIVE MANNER
 
 def fibonacci(n):
-    if n<= 0 :
+    if n<= 0 :                                  # Base case: F(0) = 0
         return 0
-    if n == 1:
+    if n == 1:                                  # Base case: F(1) = 1
         return 1
     else:
+        # Recursive case: F(n) = F(n-1) + F(n-2)
         return fibonacci(n-1) + fibonacci(n-2)
     
+# Input the value of n from the user
 n = int(input("Enter value of n: "))
 
 if  n < 0:
     print("Enter a non-negative integer")
 else:
-    result = fibonacci(n)
+    result = fibonacci(n)                        # Call the fibonacci function to calculate the Fibonacci number
     print("The fibonacci number is",result)
 
 
